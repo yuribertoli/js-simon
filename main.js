7c7b15386ccd21e9f5f48faaa1f74b5f5fac5b7f
yuribertoli <yuribertoli@yahoo.it>
2022-02-07 16:19:47 +0100

    funziona

--- main.js
+++ main.js
@@ -2,31 +2,36 @@
 const testo = document.getElementById("testo");
 const box = document.getElementById("box");
 
-//All'avvio della pagina faccio partire il gioco
-updatePage();
-
 //Inserisco un timer per far scomparire i numeri e passare alla fase 2
 setTimeout(domandaNumeri, 3000);
 
+//inizio gioco
+let arrayNumbers = []; //creo un'array dove inserire i numeri random
+
+//Inserisco casualmente 5 numeri all'aggiornarsi della pagina
+for (i=0; i<5; i++) {
+
+    let numeroCasuale = randomNumber(0, 100);
+    box.innerHTML += numeroCasuale + ` `;
+    arrayNumbers.push(numeroCasuale);
+    console.log(arrayNumbers);
+
+}
+
+//Inserisco il testo dentro l'h1
+testo.innerHTML = "Osserva i seguenti numeri per 30 secondi";
 
 
 
-
-
-
-
-
-
-
-
-let answers = [];
-let finalResult = [];
-const match = array1.filter(element => array2.includes(element));
-
+//funzione per gestire la fase 2
 function domandaNumeri() {
+
     //resetto i valori presenti sullo schermo
     testo.innerHTML = "Inserisci i 5 numeri appena visti";
     box.innerHTML = "";
+
+    //raccolgo i numeri inseriti
+    let answers = [];
 
     //lascio il tempo di leggere la frase e poi faccio partire i prompt
     setTimeout(function() {
@@ -34,39 +39,41 @@ function domandaNumeri() {
         //Per cinque volte chiedo un numero e lo aggiungo all'array answers
         for (i=0; i<5; i++) {
 
-            answers += prompt("Inserisci un numero");
-
+            let risposta = parseInt(prompt("Inserisci un numero"));
+            answers.push(risposta);
+            console.log(answers);
         }
+
+        //Controllo se i numeri inseriti corrispondono ai numeri random
+        let finalResult = trovaMatch(arrayNumbers, answers);
+        console.log(finalResult);
 
         //mostro poi i risultati sullo schermo
         testo.innerHTML = `Hai indovinato ${finalResult.length} numeri`;
         box.innerHTML = finalResult;
 
     }, 1000);
-    
+
 }
 
 
 
 
 
+//Funzione per trovare coincidenze tra 2 array
+function trovaMatch(array1, array2) {
 
-//funzione di inizio gioco
-function updatePage() {
+    let matches = [];
 
-    let arrayNumbers = [];
+    for (let i=0; i<array1.length; i++) {
 
-    //Inserisco casualmente 5 numeri all'aggiornarsi della pagina
-    for (i=0; i<5; i++) {
-        box.innerHTML += randomNumber(0, 100) + ` `;
-        arrayNumbers.push(box);
-        console.log(box);
+        for (let j=0; j<array2.length; j++) {
+
+            if (array1[i] === array2[j]) matches.push(array1[i]);
+        }
     }
 
-    //Inserisco il testo dentro l'h1
-    testo.innerHTML = "Osserva i seguenti numeri per 30 secondi";
-
-    return arrayNumbers;
+    return matches;
 }
 
 //Funzione per mostrare un numero random compreso tra 2 numeri definiti in precedenza
